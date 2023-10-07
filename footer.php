83e0f33bf655d895d5bde9ae30ae583cbe1ec746
CheechRockwizard <git@lamcat.co.uk>
2023-10-07 17:33:10 +0100

    footer changes

--- footer.php
+++ footer.php
@@ -15,15 +15,21 @@ defined('ABSPATH') || exit;
     <div class="container pt-5">
         <div class="row px-4 px-md-0">
             <div class="col-md-6 col-lg-3 text-center text-md-start mb-4">
-                <img src="<?=get_stylesheet_directory_uri()?>/img/rxmile-logo--white.png" class="mb-3" width=180 height=49>
-                <div class="text-blue-100">Control, Compliant, Cost-Effective</div>
+                <img src="<?=get_stylesheet_directory_uri()?>/img/rxmile-logo--white.png"
+                    class="mb-3" width=180 height=49>
+                <div class="text-blue-100"><a href="/control/">Control</a>, <a href="/compliant/">Compliant</a>, <a
+                        href="/cost-effective/">Cost-Effective</a></div>
                 <div class="social justify-content-center justify-content-md-start">
-                    <?=do_shortcode( '[social_icons]' )?>
+                    <?=do_shortcode('[social_icons]')?>
                 </div>
             </div>
             <div class="col-md-6 col-lg-3 mb-4 d-flex flex-column mx-auto">
-                <div class="d-flex mb-4 mx-auto mx-md-0 text-center text-md-start"><img src="<?=get_stylesheet_directory_uri()?>/img/icon--marker.svg" class="icon"> 320 W Sabal Palm Ste 300,<br>Longwood, Orlando,<br> FL 32779</div>
-                <div class="d-flex mx-auto mx-md-0"><img src="<?=get_stylesheet_directory_uri()?>/img/icon--phone.svg" class="icon"> <a href="tel:+12704796453">(270) 479-6453</a></div>
+                <div class="d-flex mb-4 mx-auto mx-md-0 text-center text-md-start"><img
+                        src="<?=get_stylesheet_directory_uri()?>/img/icon--marker.svg"
+                        class="icon"> 320 W Sabal Palm Ste 300,<br>Longwood, Orlando,<br> FL 32779</div>
+                <div class="d-flex mx-auto mx-md-0"><img
+                        src="<?=get_stylesheet_directory_uri()?>/img/icon--envelope.svg"
+                        class="icon"> <a href="mailto:inquiries@rxmile.com">inquiries@rxmile.com</a></div>
             </div>
             <div class="col-md-6 col-lg-3 text-center text-md-start mb-4">
                 <?php wp_nav_menu(array('theme_location' => 'footer_menu1')); ?>
@@ -40,7 +46,8 @@ defined('ABSPATH') || exit;
                 <a href="/privacy-policy/">Privacy Policy</a>
             </div>
             <div class="col-md-4 text-center">
-                Copyright &copy; <?=date('Y')?> RxMile
+                Copyright &copy; <?=date('Y')?>
+                RxMile
             </div>
             <div class="col-md-4 text-center text-md-end">
                 <a href="http://flpixel.com/" rel="nofollow noopener" target="_blank">Site by FL Pixel</a>
@@ -56,12 +63,13 @@ defined('ABSPATH') || exit;
 if (get_field('gtm_property', 'options')) {
     ?>
 <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=<?=get_field('gtm_property', 'options')?>" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<noscript><iframe
+        src="https://www.googletagmanager.com/ns.html?id=<?=get_field('gtm_property', 'options')?>"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
-    <?php
+<?php
 }
 ?>
 </body>
 
 </html>
-
