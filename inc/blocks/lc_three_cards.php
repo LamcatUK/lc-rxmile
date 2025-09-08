971567eac8b52c0b7579e5f5b30d58e1f761e247
LamcatUK <git@lamcat.co.uk>
2025-09-08 16:07:58 +0100

    3 cards

--- inc/blocks/lc_three_cards.php
+++ inc/blocks/lc_three_cards.php
@@ -1,35 +1,59 @@
 <?php
-$bg = get_field('background');
+/**
+ * Block Name: Three Cards
+ *
+ * This is the template that displays the Three Cards block.
+ *
+ * @package lc-rxmile
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+$bg = get_field( 'background' ) ? 'bg--' . get_field( 'background' ) : '';
 ?>
 <!-- three_cards -->
-<section class="three_cards pb-5 bg--<?=$bg?>">
+<section class="three_cards pb-5 <?= esc_attr( $bg ); ?>">
     <div class="container">
         <div class="row d-flex justify-content-center">
             <?php
-        while (have_rows('cards')) {
-            the_row();
-            $img = wp_get_attachment_image_src(get_sub_field('image'), 'large');
-            ?>
+            while ( have_rows( 'cards' ) ) {
+                the_row();
+                $img = wp_get_attachment_image_src( get_sub_field( 'image' ), 'large' );
+                ?>
             <div class="col-lg-4 py-5 py-xl-0">
                 <div class="three_cards__card">
                     <img class="three_cards__image mb-4"
-                        src="<?=$img[0]?>"
-                        width=<?=$img[1]?>
-                    height=<?=$img[2]?>>
+                        src="<?= esc_url( $img[0] ); ?>"
+                        width="<?= esc_attr( $img[1] ); ?>"
+                        height="<?= esc_attr( $img[2] ); ?>">
                     <div class="pre-title text-center">
-                        <?=get_sub_field('pre_title')?>
+                        <?= esc_html( get_sub_field( 'pre_title' ) ); ?>
                     </div>
                     <h3 class="text-center">
-                        <?=get_sub_field('title')?>
+                        <?php
+                        if ( get_sub_field( 'title_link' ) ) {
+                            ?>
+                        <a href="<?= esc_url( get_sub_field( 'title_link' ) ); ?>">
+                            <?php
+                        }
+                        ?>
+                        <?= esc_html( get_sub_field( 'title' ) ); ?>
+                        <?php
+                        if ( get_sub_field( 'title_link' ) ) {
+                            ?>
+                        </a>
+                            <?php
+                        }
+                        ?>
                     </h3>
                     <div>
-                        <?=get_sub_field('content')?>
+                        <?= wp_kses_post( get_sub_field( 'content' ) ); ?>
                     </div>
                 </div>
             </div>
-            <?php
-        }
-?>
+                <?php
+            }
+            ?>
         </div>
     </div>
 </section>
