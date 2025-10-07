0913f0a0dac7f33d8d0a6be47a947236d1e35f4b
LamcatUK <git@lamcat.co.uk>
2025-10-07 20:29:24 +0100

    partner integrations

--- blocks/lc_integration_partners.php
+++ blocks/lc_integration_partners.php
@@ -5,11 +5,11 @@ defined( 'ABSPATH' ) || exit;
 ?>
 <section class="integration-partners">
 	<div class="container py-5">
-		<h2><?= esc_html( get_field( 'title' ) ); ?></h2>
-		<div class="mb-4">
+		<h2 class="text-center"><?= esc_html( get_field( 'title' ) ); ?></h2>
+		<div class="mb-5 text-center">
 			<?= wp_kses_post( get_field( 'intro' ) ); ?>
 		</div>
-		<div class="d-flex justify-content-center align-items-center gap-4">
+		<div class="d-flex flex-wrap justify-content-center align-items-center gap-4">
 			<?php
 			foreach ( get_field( 'partner_pages' ) as $partner ) {
 				?>
