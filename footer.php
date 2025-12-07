8b424b520f9351b2c244a2283a984de730632177
LamcatUK <git@lamcat.co.uk>
2025-12-07 15:12:16 +0000

    modal

--- footer.php
+++ footer.php
@@ -12,77 +12,121 @@ defined('ABSPATH') || exit;
 
 ?>
 <div class="footer pb-3">
-    <div class="container pt-5">
-        <div class="row px-4 px-md-0">
-            <div class="col-md-6 col-lg-3 text-center text-md-start mb-4">
-                <img src="<?=get_stylesheet_directory_uri()?>/img/rxmile-logo--white.png"
-                    class="mb-3" width=180 height=49>
-                <div class="text-blue-100"><a href="/control/">Control</a>, <a href="/compliant/">Compliant</a>, <a
-                        href="/cost-effective/">Cost-Effective</a></div>
-                <div class="social justify-content-center justify-content-md-start">
-                    <?=do_shortcode('[social_icons]')?>
-                </div>
-            </div>
-            <div class="col-md-6 col-lg-3 mb-4 d-flex flex-column mx-auto">
-                <div class="d-flex mb-4 mx-auto mx-md-0 text-center text-md-start"><img
-                        src="<?=get_stylesheet_directory_uri()?>/img/icon--marker.svg"
-                        class="icon"> 320 W Sabal Palm Ste 300,<br>Longwood, Orlando,<br> FL 32779</div>
-                <div class="d-flex mx-auto mx-md-0"><img
-                        src="<?=get_stylesheet_directory_uri()?>/img/icon--envelope.svg"
-                        class="icon"> <a href="mailto:inquiries@rxmile.com">inquiries@rxmile.com</a></div>
-            </div>
-            <div class="col-md-6 col-lg-3 text-center text-md-start mb-4">
-                <?php wp_nav_menu(array('theme_location' => 'footer_menu1')); ?>
-            </div>
-            <div class="col-md-6 col-lg-3 text-center text-md-start mb-4">
-                <?php wp_nav_menu(array('theme_location' => 'footer_menu2')); ?>
-            </div>
-        </div>
-    </div>
-    <div class="container py-2 colophon">
-        <div class="row">
-            <div class="col-md-4 text-center text-md-start">
-                <a href="/terms-conditions/">Terms &amp; Conditions</a>&nbsp;
-                <a href="/privacy-policy/">Privacy Policy</a>
-            </div>
-            <div class="col-md-4 text-center">
-                Copyright &copy; <?=date('Y')?>
-                RxMile
-            </div>
-            <div class="col-md-4 text-center text-md-end">
-                <a href="http://flpixel.com/" rel="nofollow noopener" target="_blank">Site by FL Pixel</a>
-            </div>
-        </div>
-    </div>
+	<div class="container pt-5">
+		<div class="row px-4 px-md-0">
+			<div class="col-md-6 col-lg-3 text-center text-md-start mb-4">
+				<img src="<?= esc_url( get_stylesheet_directory_uri() ); ?>/img/rxmile-logo--white.png"
+					class="mb-3" width=180 height=49>
+				<div class="text-blue-100"><a href="/control/">Control</a>, <a href="/compliant/">Compliant</a>, <a
+						href="/cost-effective/">Cost-Effective</a></div>
+				<div class="social justify-content-center justify-content-md-start">
+					<?= do_shortcode('[social_icons]'); ?>
+				</div>
+			</div>
+			<div class="col-md-6 col-lg-3 mb-4 d-flex flex-column mx-auto">
+				<div class="d-flex mb-4 mx-auto mx-md-0 text-center text-md-start"><img
+						src="<?= esc_url( get_stylesheet_directory_uri() ); ?>/img/icon--marker.svg"
+						class="icon"> 320 W Sabal Palm Ste 300,<br>Longwood, Orlando,<br> FL 32779</div>
+				<div class="d-flex mx-auto mx-md-0"><img
+						src="<?= esc_url( get_stylesheet_directory_uri() ); ?>/img/icon--envelope.svg"
+						class="icon"> <a href="mailto:inquiries@rxmile.com">inquiries@rxmile.com</a></div>
+			</div>
+			<div class="col-md-6 col-lg-3 text-center text-md-start mb-4">
+				<?php wp_nav_menu( array( 'theme_location' => 'footer_menu1' ) ); ?>
+			</div>
+			<div class="col-md-6 col-lg-3 text-center text-md-start mb-4">
+				<?php wp_nav_menu( array( 'theme_location' => 'footer_menu2' ) ); ?>
+			</div>
+		</div>
+	</div>
+	<div class="container py-2 colophon">
+		<div class="row">
+			<div class="col-md-4 text-center text-md-start">
+				<a href="/terms-conditions/">Terms &amp; Conditions</a>&nbsp;
+				<a href="/privacy-policy/">Privacy Policy</a>
+			</div>
+			<div class="col-md-4 text-center">
+				Copyright &copy; <?= gmdate( 'Y' ); ?>
+				RxMile
+			</div>
+			<div class="col-md-4 text-center text-md-end">
+				<a href="http://flpixel.com/" rel="nofollow noopener" target="_blank">Site by FL Pixel</a>
+			</div>
+		</div>
+	</div>
 </div>
 </div><!-- #page -->
 
 <div class="modal fade" id="choiceModal" tabindex="-1" role="dialog" aria-labelledby="choiceModalLabel"
-    aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" style="max-width:min(600px,90vw)" role="document">
-        <div class="modal-content">
-            <div class="modal-body">
-                <div class="row two_cards g-4">
-                    <div class="col-md-6">
-                        <div class="two_cards__card text-center" type="button"
-                            onclick="Calendly.initPopupWidget({url: 'https://calendly.com/rxmile/pharmacy-demo?month=<?=date('YYYY')?>-<?=date('m')?>'});return false;">
-                            <div class="h3">For Pharmacies</div>
-                            <div class="mb-4">Schedule your pharmacy delivery software demo here.</div>
-                            <div class="btn btn-primary btn--small mb-2">Book Demo</div>
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="two_cards__card text-center" type="button"
-                            onclick="Calendly.initPopupWidget({url: 'https://calendly.com/francis_uy/start-your-medical-courier-business-today?month=<?=date('YYYY')?>-<?=date('m')?>' });return false;">
-                            <div class="h3">For Couriers</div>
-                            <div class="mb-4">Schedule a demo to join our courier network here.</div>
-                            <div class="btn btn-navbar mb-2 fw-bold" style="min-width:190px">Book Demo</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+	aria-hidden="true">
+	<div class="modal-dialog modal-dialog-centered" style="max-width:min(600px,90vw)" role="document">
+		<div class="modal-content">
+			<div class="modal-body">
+				<div class="row two_cards g-4">
+					<div class="col-md-6">
+						<div class="two_cards__card text-center" type="button" data-bs-toggle="modal" data-bs-target="#pharmacyModal" data-bs-dismiss="modal">
+							<div class="h3">For Pharmacies</div>
+							<div class="mb-4">Schedule your pharmacy delivery software demo here.</div>
+							<div class="btn btn-primary btn--small mb-2">Book Demo</div>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="two_cards__card text-center" type="button" data-bs-toggle="modal" data-bs-target="#courierModal" data-bs-dismiss="modal">
+							<div class="h3">For Couriers</div>
+							<div class="mb-4">Schedule a demo to join our courier network here.</div>
+							<div class="btn btn-navbar mb-2 fw-bold" style="min-width:190px">Book Demo</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- Pharmacy HubSpot Form Modal -->
+<div class="modal fade" id="pharmacyModal" tabindex="-1" role="dialog" aria-labelledby="pharmacyModalLabel" aria-hidden="true">
+	<div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="pharmacyModalLabel">For Pharmacies</h5>
+				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+			</div>
+			<div class="modal-body">
+				<!-- Replace with your HubSpot form embed code for Pharmacies -->
+				<script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/embed/v2.js"></script>
+				<script>
+				hbspt.forms.create({
+					region: "na1",
+					portalId: "4968908",
+					formId: "af16aae5-9b98-48d8-be60-12777f90d28d"
+				});
+				</script>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- Courier HubSpot Form Modal -->
+<div class="modal fade" id="courierModal" tabindex="-1" role="dialog" aria-labelledby="courierModalLabel" aria-hidden="true">
+	<div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="courierModalLabel">For Couriers</h5>
+				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+			</div>
+			<div class="modal-body">
+				<!-- Replace with your HubSpot form embed code for Couriers -->
+				<script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/embed/v2.js"></script>
+				<script>
+				hbspt.forms.create({
+					region: "na1",
+					portalId: "4968908",
+					formId: "f546eeac-a6c9-4257-ab9f-e34f2e1aa5d9"
+				});
+				</script>
+			</div>
+		</div>
+	</div>
 </div>
 
 
@@ -90,15 +134,16 @@ defined('ABSPATH') || exit;
 <!-- <script src="https://links.digivox.ai/js/form_embed.js"></script> -->
 <script src="https://assets.calendly.com/assets/external/widget.js" type="text/javascript" async></script>
 <!-- <div class="enquire-button">Enquire</div> -->
-<?php wp_footer();
-if (get_field('gtm_property', 'options')) {
-    ?>
+<?php
+wp_footer();
+if ( get_field( 'gtm_property', 'options' ) ) {
+	?>
 <!-- Google Tag Manager (noscript) -->
 <noscript><iframe
-        src="https://www.googletagmanager.com/ns.html?id=<?=get_field('gtm_property', 'options')?>"
-        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+		src="https://www.googletagmanager.com/ns.html?id=<?= get_field( 'gtm_property', 'options' ); ?>"
+		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
-<?php
+	<?php
 }
 ?>
 </body>
--- header.php
+++ header.php
@@ -13,176 +13,176 @@ defined( 'ABSPATH' ) || exit;
 <html <?php language_attributes(); ?>>
 
 <head>
-    <meta
-        charset="<?php bloginfo( 'charset' ); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="profile" href="http://gmpg.org/xfn/11">
-    <?php
-    if ( get_field( 'ga_property', 'options' ) ) {
-        ?>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async
-        src="https://www.googletagmanager.com/gtag/js?id=<?= esc_attr( get_field( 'ga_property', 'options' ) ); ?>">
-    </script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
+	<meta
+		charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<?php
+	if ( get_field( 'ga_property', 'options' ) ) {
+		?>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async
+		src="https://www.googletagmanager.com/gtag/js?id=<?= esc_attr( get_field( 'ga_property', 'options' ) ); ?>">
+	</script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
 
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-        gtag('js', new Date());
-        gtag('config',
-            '<?= esc_attr( get_field( 'ga_property', 'options' ) ); ?>'
-        );
-    </script>
-        <?php
-    }
-    if ( get_field( 'gtm_property', 'options' ) ) {
-        ?>
-    <!-- Google Tag Manager -->
-    <script>
-        (function(w, d, s, l, i) {
-            w[l] = w[l] || [];
-            w[l].push({
-                'gtm.start': new Date().getTime(),
-                event: 'gtm.js'
-            });
-            var f = d.getElementsByTagName(s)[0],
-                j = d.createElement(s),
-                dl = l != 'dataLayer' ? '&l=' + l : '';
-            j.async = true;
-            j.src =
-                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-            f.parentNode.insertBefore(j, f);
-        })(window, document, 'script', 'dataLayer',
-            '<?= esc_attr( get_field( 'gtm_property', 'options' ) ); ?>'
-        );
-    </script>
-    <!-- End Google Tag Manager -->
-        <?php
-    }
-    if ( get_field( 'google_site_verification', 'options' ) ) {
-        echo '<meta name="google-site-verification" content="' . esc_attr( get_field( 'google_site_verification', 'options' ) ) . '" />';
-    }
-    if ( get_field( 'bing_site_verification', 'options' ) ) {
-        echo '<meta name="msvalidate.01" content="' . esc_attr( get_field( 'bing_site_verification', 'options' ) ) . '" />';
-    }
-    ?>
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+		gtag('config',
+			'<?= esc_attr( get_field( 'ga_property', 'options' ) ); ?>'
+		);
+	</script>
+		<?php
+	}
+	if ( get_field( 'gtm_property', 'options' ) ) {
+		?>
+	<!-- Google Tag Manager -->
+	<script>
+		(function(w, d, s, l, i) {
+			w[l] = w[l] || [];
+			w[l].push({
+				'gtm.start': new Date().getTime(),
+				event: 'gtm.js'
+			});
+			var f = d.getElementsByTagName(s)[0],
+				j = d.createElement(s),
+				dl = l != 'dataLayer' ? '&l=' + l : '';
+			j.async = true;
+			j.src =
+				'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+			f.parentNode.insertBefore(j, f);
+		})(window, document, 'script', 'dataLayer',
+			'<?= esc_attr( get_field( 'gtm_property', 'options' ) ); ?>'
+		);
+	</script>
+	<!-- End Google Tag Manager -->
+		<?php
+	}
+	if ( get_field( 'google_site_verification', 'options' ) ) {
+		echo '<meta name="google-site-verification" content="' . esc_attr( get_field( 'google_site_verification', 'options' ) ) . '" />';
+	}
+	if ( get_field( 'bing_site_verification', 'options' ) ) {
+		echo '<meta name="msvalidate.01" content="' . esc_attr( get_field( 'bing_site_verification', 'options' ) ) . '" />';
+	}
+	?>
 	<!-- Start of HubSpot Embed Code -->
 	<script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/4968908.js"></script>
 	<!-- End of HubSpot Embed Code -->
-    <link href="https://assets.calendly.com/assets/external/widget.css" rel="stylesheet">
-    <!-- Meta Pixel Code -->
-    <script>
-        ! function(f, b, e, v, n, t, s) {
-            if (f.fbq) return;
-            n = f.fbq = function() {
-                n.callMethod ?
-                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-            };
-            if (!f._fbq) f._fbq = n;
-            n.push = n;
-            n.loaded = !0;
-            n.version = '2.0';
-            n.queue = [];
-            t = b.createElement(e);
-            t.async = !0;
-            t.src = v;
-            s = b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t, s)
-        }(window, document, 'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '2614397218698886');
-        fbq('track', 'PageView');
-    </script>
-    <noscript><img height="1" width="1" alt="" style="display:none"
-            src="https://www.facebook.com/tr?id=2614397218698886&ev=PageView&noscript=1" /></noscript>
-    <!-- End Meta Pixel Code -->
-    <?php wp_head(); ?>
+	<link href="https://assets.calendly.com/assets/external/widget.css" rel="stylesheet">
+	<!-- Meta Pixel Code -->
+	<script>
+		! function(f, b, e, v, n, t, s) {
+			if (f.fbq) return;
+			n = f.fbq = function() {
+				n.callMethod ?
+					n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+			};
+			if (!f._fbq) f._fbq = n;
+			n.push = n;
+			n.loaded = !0;
+			n.version = '2.0';
+			n.queue = [];
+			t = b.createElement(e);
+			t.async = !0;
+			t.src = v;
+			s = b.getElementsByTagName(e)[0];
+			s.parentNode.insertBefore(t, s)
+		}(window, document, 'script',
+			'https://connect.facebook.net/en_US/fbevents.js');
+		fbq('init', '2614397218698886');
+		fbq('track', 'PageView');
+	</script>
+	<noscript><img height="1" width="1" alt="" style="display:none"
+			src="https://www.facebook.com/tr?id=2614397218698886&ev=PageView&noscript=1" /></noscript>
+	<!-- End Meta Pixel Code -->
+	<?php wp_head(); ?>
 
-    <script type="application/ld+json">
-        {
-            "@context": "https://schema.org",
-            "@type": "Organization",
-            "name": "RxMile",
-            "legalName": "Zek Tech LLC",
-            "url": "https://www.rxmile.com/",
-            "logo": "https://www.rxmile.com/wp-content/uploads/2022/03/RxMile-Logo.png",
-            "foundingDate": "2022",
-            "founders": [{
-                "@type": "Person",
-                "name": "Kunal Vyas"
-            }],
-            "address": {
-                "@type": "PostalAddress",
-                "streetAddress": "320 W Sabal Palm Ste 300",
-                "addressLocality": "Longwood",
-                "addressRegion": "Orlando, Florida",
-                "postalCode": "32803",
-                "addressCountry": "USA"
-            },
-            "contactPoint": {
-                "@type": "ContactPoint",
-                "contactType": "customer support",
-                "telephone": "[270 4796 453]",
-                "email": "inquiries@rxmile.com"
-            },
-            "sameAs": [
-                "https://www.facebook.com/RxMileSaaS",
-                "https://www.linkedin.com/company/rxmile/",
-                "https://www.youtube.com/@rxmile2497"
-            ]
-        }
-    </script>
+	<script type="application/ld+json">
+		{
+			"@context": "https://schema.org",
+			"@type": "Organization",
+			"name": "RxMile",
+			"legalName": "Zek Tech LLC",
+			"url": "https://www.rxmile.com/",
+			"logo": "https://www.rxmile.com/wp-content/uploads/2022/03/RxMile-Logo.png",
+			"foundingDate": "2022",
+			"founders": [{
+				"@type": "Person",
+				"name": "Kunal Vyas"
+			}],
+			"address": {
+				"@type": "PostalAddress",
+				"streetAddress": "320 W Sabal Palm Ste 300",
+				"addressLocality": "Longwood",
+				"addressRegion": "Orlando, Florida",
+				"postalCode": "32803",
+				"addressCountry": "USA"
+			},
+			"contactPoint": {
+				"@type": "ContactPoint",
+				"contactType": "customer support",
+				"telephone": "[270 4796 453]",
+				"email": "inquiries@rxmile.com"
+			},
+			"sameAs": [
+				"https://www.facebook.com/RxMileSaaS",
+				"https://www.linkedin.com/company/rxmile/",
+				"https://www.youtube.com/@rxmile2497"
+			]
+		}
+	</script>
 
 </head>
 
 <body <?php body_class(); ?>
-    <?php understrap_body_attributes(); ?>>
-    <?php do_action( 'wp_body_open' ); ?>
+	<?php understrap_body_attributes(); ?>>
+	<?php do_action( 'wp_body_open' ); ?>
 <header id="wrapper-navbar" class="fixed-top p-0">
 	<nav class="navbar navbar-expand-lg">
 		<div class="container gap-4">
-            <div class="d-flex justify-content-between w-100 w-lg-auto align-items-center py-3 py-lg-0">
-                <div class="logo-container"><a href="/" class="logo navbar-brand" aria-label="RxMile Homepage"></a></div>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <i class="fas fa-bars"></i>
-                </button>
-            </div>
-            <div id="navbar" class="collapse navbar-collapse">
-                <div class="w-100 d-flex flex-column justify-content-lg-end align-items-lg-center">
-                    <!-- Contact Details (Hidden on Mobile) -->
-                    <div class="contact-info d-none d-lg-flex gap-3 w-100 justify-content-end align-items-center pb-2">
-                        <?php
-                        if ( get_field( 'header_button', 'options' ) ) {
-                            $l = get_field( 'header_button', 'options' );
-                            ?>
-                        <span type="button" class="btn-navbar--outline" data-bs-toggle="modal"
-                            data-bs-target="#choiceModal">
-                            Book Demo
-                        </span>
-                            <?php
-                        }
-                        ?>
-                        <a href="https://tracker.rxmile.com/" class="btn-navbar">Login</a>
-                    </div>
-                    <!-- Navigation -->
-                    <?php
-                    wp_nav_menu(
-                        array(
-                            'theme_location' => 'primary_nav',
-                            'container'      => false,
-                            'menu_class'     => 'navbar-nav w-100 justify-content-end gap-5',
-                            'fallback_cb'    => '',
-                            'depth'          => 3,
-                            'walker'         => new Understrap_WP_Bootstrap_Navwalker(),
-                        )
-                    );
-                    ?>
-                </div>
-            </div>
+			<div class="d-flex justify-content-between w-100 w-lg-auto align-items-center py-3 py-lg-0">
+				<div class="logo-container"><a href="/" class="logo navbar-brand" aria-label="RxMile Homepage"></a></div>
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+					data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false"
+					aria-label="Toggle navigation">
+					<i class="fas fa-bars"></i>
+				</button>
+			</div>
+			<div id="navbar" class="collapse navbar-collapse">
+				<div class="w-100 d-flex flex-column justify-content-lg-end align-items-lg-center">
+					<!-- Contact Details (Hidden on Mobile) -->
+					<div class="contact-info d-none d-lg-flex gap-3 w-100 justify-content-end align-items-center pb-2">
+						<?php
+						if ( get_field( 'header_button', 'options' ) ) {
+							$l = get_field( 'header_button', 'options' );
+							?>
+						<span type="button" class="btn-navbar--outline" data-bs-toggle="modal"
+							data-bs-target="#choiceModal">
+							Book Demo
+						</span>
+							<?php
+						}
+						?>
+						<a href="https://tracker.rxmile.com/" class="btn-navbar">Login</a>
+					</div>
+					<!-- Navigation -->
+					<?php
+					wp_nav_menu(
+						array(
+							'theme_location' => 'primary_nav',
+							'container'      => false,
+							'menu_class'     => 'navbar-nav w-100 justify-content-end gap-5',
+							'fallback_cb'    => '',
+							'depth'          => 3,
+							'walker'         => new Understrap_WP_Bootstrap_Navwalker(),
+						)
+					);
+					?>
+				</div>
+			</div>
 		</div>
 	</nav>
 </header>
